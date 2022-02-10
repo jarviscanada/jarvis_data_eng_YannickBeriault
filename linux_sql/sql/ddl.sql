0ec9630f148e4ac024d131ec785d6e3b76701859
Yannick Bériault <beriault.yannick@gmail.com>
2022-02-10 15:34:46 +0000

    Simplified the ddl.sql script. I'm letting the DB do the job of checking if the tables already exist.

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,40 +1,23 @@
+CREATE TABLE PUBLIC.host_info
+(
+    id                  SERIAL NOT NULL,
+    hostname            VARCHAR NOT NULL,
+    cpu_number          INT,
+    cpu_architecture    VARCHAR(50),
+    cpu_model           VARCHAR(100),
+    cpu_mhz             FLOAT,
+    L2_cache            INT,
+    total_mem           INT,
+    timestamp           TIMESTAMP
+);
 
-IF (EXISTS (SELECT *
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = 'public' AND TABLE_NAME = 'host_info'
-    )
-)
-BEGIN
-
-    CREATE TABLE PUBLIC.host_info
-    (
-        id                  SERIAL NOT NULL,
-        hostname            VARCHAR NOT NULL,
-        cpu_number          INT,
-        cpu_architecture    VARCHAR(50),
-        cpu_model           VARCHAR(100),
-        cpu_mhz             FLOAT,
-        L2_cache            INT,
-        total_mem           INT,
-        timestamp           TIMESTAMP
-    );
-END
-
-IF (EXISTS (SELECT *
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_SCHEMA = 'public' AND TABLE_NAME = 'host_usage'
-    )
-)
-BEGIN
-
-    CREATE TABLE PUBLIC.host_usage
-    (
-        timestamp           TIMESTAMP NOT NULL,
-        host_id             SERIAL NOT NULL,
-        memory_free         INT,
-        cpu_idle            SMALLINT,
-        cpu_kernel          SMALLINT,
-        disk_io             INT,
-        disk_available      INT
-    );
-END
+CREATE TABLE PUBLIC.host_usage
+(
+    timestamp           TIMESTAMP NOT NULL,
+    host_id             SERIAL NOT NULL,
+    memory_free         INT,
+    cpu_idle            SMALLINT,
+    cpu_kernel          SMALLINT,
+    disk_io             INT,
+    disk_available      INT
+);
