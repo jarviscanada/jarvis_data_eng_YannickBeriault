0e1af5b09d93e1bb1873c718982a7c7b49485225
Yannick Bériault <beriault.yannick@gmail.com>
2022-02-11 22:05:27 +0000

    host_usage.sh drafted and working

--- linux_sql/README.md
+++ linux_sql/README.md
@@ -1,2 +1,4 @@
 # Linux Cluster Monitoring Agent
 This project is under development. Since this project follows the GitFlow, the final work will be merged to the master branch after Team Code Team.
+
+A note for later: date is in UTC. Check if that is the time to use.
--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -21,9 +21,8 @@ fi
 hostname=$(hostname -f)
 export PGPASSWORD=$psql_password
 host_id_query="SELECT id FROM host_info WHERE hostname = '$hostname';"
-host_id="$(psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$host_id_query" | grep row | sed 's/[^0-9]//g')"
-
-echo $host_id
+host_id="$(psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$host_id_query" | grep row \
+    | sed 's/[^0-9]//g')"
 
 if [ $host_id -eq 0 ]; then
 
@@ -34,17 +33,14 @@ if [ $host_id -eq 0 ]; then
   #save full model name
   fullModelName="$(echo "$lscpu_out" | grep "Model name")"
 
-  #save memory data
-  memoryData="$(cat /proc/meminfo)"
-
   #hardware
   hostname="$(hostname -f)"
   cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
   cpu_architecture="$(echo "$lscpu_out" | grep Architecture | cut - -f 2 -d ':' | xargs)"
   cpu_model="$(echo "$fullModelName" | cut - -f 2 -d ':' | cut - -f 1 -d '@' | xargs)"
   cpu_mhz="$(echo "$lscpu_out" | grep "CPU MHz" | cut - -f 2 -d ':' | xargs)"
-  l2_cache="$(echo "$lscpu_out" | grep "L2 cache" | cut - -f 2 -d ':' | xargs | sed 's/K//')"
-  total_mem="$(cat /proc/meminfo | grep MemTotal | cut - -f 2 -d ':' | xargs | sed 's/ kB//')"
+  l2_cache="$(echo "$lscpu_out" | grep "L2 cache" | cut - -f 2 -d ':' | xargs | sed 's/[^0-9]//g')"
+  total_mem="$(cat /proc/meminfo | grep MemTotal | cut - -f 2 -d ':' | xargs | sed 's/[^0-9]//g')"
   timestamp="$(date +"%Y-%m-%d %H:%M:%S")"
 
   #Inserting server usage data into host_usage table
