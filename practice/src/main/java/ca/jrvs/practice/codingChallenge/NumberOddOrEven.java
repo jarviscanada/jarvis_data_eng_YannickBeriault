83664f192884b09f6b3cf61069af4f99045f4afa
YannickBeriault <beriault.yannick@gmail.com>
2022-03-07 15:35:00 -0500

    Number odd or even with modulo solution

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/NumberOddOrEven.java
@@ -0,0 +1,35 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * https://www.notion.so/jarvisdev/Sample-Check-if-a-number-is-even-or-odd-f8f06cb4f11d483fa0424e0c90992560
+ */
+
+public class NumberOddOrEven {
+
+    public static String checkWithModulo(int numberToCheck) {
+
+        String answer;
+
+        if (numberToCheck % 2 == 0) {
+            answer = "even";
+        } else {
+            answer = "odd";
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int numberToCheck;
+
+        try {
+            numberToCheck = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Argument must be an integer!");
+            return;
+        }
+
+        System.out.println(checkWithModulo(numberToCheck));
+    }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/NumberOddOrEvenTest.java
@@ -0,0 +1,29 @@
+package ca.jrvs.practice.codingChallenge;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberOddOrEvenTest {
+
+    @Test
+    public void TestCheckWithModuloEven() {
+        String answer;
+        String expected = "even";
+
+        answer = NumberOddOrEven.checkWithModulo(8);
+
+        assertEquals(answer, expected);
+    }
+
+    @Test
+    public void TestCheckWithModuloOdd() {
+        String answer;
+        String expected = "odd";
+
+        answer = NumberOddOrEven.checkWithModulo(15);
+
+        assertEquals(answer, expected);
+    }
+}
