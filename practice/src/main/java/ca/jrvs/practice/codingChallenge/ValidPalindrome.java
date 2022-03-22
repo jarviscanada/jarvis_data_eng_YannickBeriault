b19abe5f7e0321fe79bc2f9e6a93ca4636623063
YannickBeriault <beriault.yannick@gmail.com>
2022-03-22 17:27:58 -0400

    Completed the Valid Palindromes coding challenge.

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidPalindrome.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidPalindrome.java
@@ -5,7 +5,77 @@ import java.util.ListIterator;
 
 public class ValidPalindrome {
 
+    private static String workingS;
+
     public static boolean isPalindrome(String s) {
+
+        if (s.length() <= 1)
+            return true;
+        else {
+
+            workingS = s;
+            return fastIsPalindromeRecursive(0, s.length());
+        }
+    }
+
+    private static boolean fastIsPalindromeRecursive(int head, int tail) {
+
+        if (head >= tail)
+            return true;
+
+        if (!Character.isLetterOrDigit(workingS.charAt(head)))
+            return fastIsPalindromeRecursive(head + 1, tail);
+        if (!Character.isLetterOrDigit(workingS.charAt(tail)))
+            return fastIsPalindromeRecursive(head, tail - 1);
+
+        if (Character.toLowerCase(workingS.charAt(head))
+                == Character.toLowerCase(workingS.charAt(tail))) {
+
+            if (tail == 1)
+                return true;
+            else
+                return fastIsPalindromeRecursive( head + 1, tail - 1);
+        } else
+            return false;
+    }
+
+    public static boolean isPalindromeSlow(String s) {
+
+        if (s.length() <= 1)
+            return true;
+
+        StringBuilder workString = new StringBuilder(s);
+
+        return isPalindromeRecursive(workString);
+    }
+
+    private static boolean isPalindromeRecursive(StringBuilder workString) {
+
+        int tailIndex = workString.length() - 1;
+        if (tailIndex == 0)
+            return true;
+
+        if (!Character.isLetterOrDigit(workString.charAt(0)))
+            return isPalindromeRecursive(workString.deleteCharAt(0));
+        if (!Character.isLetterOrDigit(workString.charAt(tailIndex)))
+            return isPalindromeRecursive(workString.deleteCharAt(tailIndex));
+
+        if (Character.toLowerCase(workString.charAt(0))
+                == Character.toLowerCase(workString.charAt(tailIndex))) {
+
+            if (tailIndex == 1)
+                return true;
+            else {
+                workString = workString.deleteCharAt(tailIndex);
+                return isPalindromeRecursive(workString.deleteCharAt(0));
+            }
+        } else
+            return false;
+    }
+
+    //By "pointers" I did not think what was meant was really indices. That is why I devised a solution using iterators
+    //The idea is similar to what it would be with indices, plus the work done to build the list.
+    public static boolean isPalindromeWastefulPointers(String s) {
 
         boolean itIs = false;
         LinkedList<Character> cleanedS = new LinkedList<>();
@@ -15,7 +85,7 @@ public class ValidPalindrome {
         for (int i = 0; i < s.length(); i++) {
 
             char currentChar = s.charAt(i);
-            if (Character.isAlphabetic(currentChar) || Character.isDigit(currentChar))
+            if (Character.isLetterOrDigit(currentChar))
                 cleanedS.add(Character.toLowerCase(currentChar));
         }
 
--- practice/src/test/java/ca/jrvs/practice/codingChallenge/ValidPalindromeTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ValidPalindromeTest.java
@@ -8,7 +8,11 @@ public class ValidPalindromeTest {
 
     @Test
     public void testPointerIsPalindrome() {
+        assertTrue(ValidPalindrome.isPalindromeWastefulPointers("A man, a plan, a canal: Panama"));
+    }
 
+    @Test
+    public void testIsPalindromeRecursive() {
         assertTrue(ValidPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
