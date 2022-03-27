a59ad1dd5d99a0cc931420aec17517d437f39116
YannickBeriault <beriault.yannick@gmail.com>
2022-03-27 15:14:36 -0400

    Created basic LinkedJList class.

---
+++ practice/src/main/java/ca/jrvs/practice/dataStructure/list/LinkedJList.java
@@ -0,0 +1,230 @@
+package ca.jrvs.practice.dataStructure.list;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class LinkedJList implements JList {
+
+    public ListNode firstNode;
+    public ListNode lastNode;
+    private int listSize;
+
+    public LinkedJList() {
+
+        this.firstNode = this.lastNode = null;
+        this.listSize = 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+
+        ListNode newNode = new ListNode(o);
+
+        if (firstNode == null)
+            firstNode = lastNode = newNode;
+        else {
+
+            lastNode.setNextNode(newNode);
+            lastNode = newNode;
+        }
+
+        setSize(size() + 1);
+
+        return true;
+    }
+
+    @Override
+    public Object[] toArray() {
+
+        Object[] arrayFromList = new Object[size()];
+        LinkedJListIterator<Object> iterator = new LinkedJListIterator<>();
+        int index = 0;
+
+        while (iterator.hasNext()) {
+
+            arrayFromList[index] = iterator.next();
+            index++;
+        }
+
+        return arrayFromList;
+    }
+
+    @Override
+    public int size() {
+        return listSize;
+    }
+
+    private void setSize(int newSize) {
+        listSize = newSize;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return firstNode == null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+
+        int index = 0;
+        LinkedJListIterator<Object> iterator = new LinkedJListIterator<>();
+
+        while (iterator.hasNext()) {
+
+            if (iterator.next().equals(o))
+                return index;
+            else
+                index++;
+        }
+
+        return -1;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+
+        LinkedJListIterator<Object> iterator = new LinkedJListIterator<>();
+
+        while (iterator.hasNext()) {
+
+            if (iterator.next().equals(o))
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+
+        if (index < 0)
+            throw new IndexOutOfBoundsException();
+
+        Object toReturn = null;
+        LinkedJListIterator<Object> iterator = new LinkedJListIterator<>();
+
+        while (index >= 0) {
+
+            if (!iterator.hasNext())
+                throw new IndexOutOfBoundsException();
+
+            toReturn = iterator.next();
+            index--;
+        }
+
+        return toReturn;
+    }
+
+    @Override
+    public Object remove(int index) {
+
+        ListNode currentNode;
+        ListNode previousNode;
+
+        if (index < 0)
+            throw new IndexOutOfBoundsException();
+        else if (index == 0) {
+
+            Object toReturn = firstNode.getContent();
+            firstNode = firstNode.getNextNode();
+            setSize(size() - 1);
+
+            return toReturn;
+        }
+
+        currentNode = previousNode = firstNode;
+
+        while (index > 0) {
+
+            previousNode = currentNode;
+            currentNode = currentNode.getNextNode();
+            index--;
+
+            if (currentNode == null)
+                throw new IndexOutOfBoundsException();
+        }
+
+        previousNode.setNextNode(currentNode.getNextNode());
+        setSize(size() - 1);
+
+        return currentNode.getContent();
+    }
+
+    @Override
+    public void clear() {
+
+        firstNode = lastNode = null;
+        setSize(0);
+    }
+
+    public class ListNode {
+
+        private Object content;
+        private ListNode nextNode;
+
+        public ListNode(Object o) {
+            this.content = o;
+            this.nextNode = null;
+        }
+
+        public Object getContent() {
+            return content;
+        }
+
+        public void setContent(Object o) {
+            this.content = o;
+        }
+
+        public ListNode getNextNode() {
+            return this.nextNode;
+        }
+
+        public void setNextNode(ListNode node) {
+            this.nextNode = node;
+        }
+    }
+
+    public class LinkedJListIterator<E> implements Iterator<E> {
+
+        private ListNode headNode;
+        private ListNode currentNode;
+
+        public LinkedJListIterator() {
+
+            currentNode = headNode = new ListNode(null);
+            headNode.setNextNode(firstNode);
+        }
+
+        public ListNode getCurrentNode() {
+            return currentNode;
+        }
+
+        public ListNode getNextNode() {
+            return currentNode.getNextNode();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentNode.getNextNode() != null;
+        }
+
+        @Override
+        public E next() {
+
+            currentNode = currentNode.getNextNode();
+            E toReturn = (E) currentNode.getContent();
+
+            return toReturn;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+            Iterator.super.forEachRemaining(action);
+        }
+    }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/dataStructure/list/LinkedJListTest.java
@@ -0,0 +1,107 @@
+package ca.jrvs.practice.dataStructure.list;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedJListTest {
+
+    @Test
+    public void testSizeAndAdd() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        assertEquals(linkedJList.size(), 0);
+
+        linkedJList.add(8);
+        assertEquals(linkedJList.size(), 1);
+    }
+
+    @Test
+    public void testAddAndGet() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        linkedJList.add(8);
+        assertEquals(linkedJList.get(0), 8);
+    }
+
+    @Test
+    public void testToArray() {
+
+        Integer[] expected = {7, 6, 9};
+        LinkedJList linkedJList = new LinkedJList();
+
+        linkedJList.add(7);
+        linkedJList.add(6);
+        linkedJList.add(9);
+
+        assertArrayEquals(linkedJList.toArray(), expected);
+    }
+
+    @Test
+    public void testRemoveAndIsEmpty() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        assertTrue(linkedJList.isEmpty());
+
+        linkedJList.add(9);
+        assertFalse(linkedJList.isEmpty());
+
+        linkedJList.remove(0);
+        assertTrue(linkedJList.isEmpty());
+    }
+
+    @Test
+    public void testIndexOf() {
+
+        LinkedJList linkedJList = new LinkedJList();
+
+        linkedJList.add(7);
+        linkedJList.add(6);
+        linkedJList.add(9);
+
+        assertEquals(linkedJList.indexOf(6), 1);
+        assertEquals(linkedJList.indexOf(1), -1);
+    }
+
+    @Test
+    public void testContains() {
+
+        LinkedJList linkedJList = new LinkedJList();
+
+        linkedJList.add(7);
+        linkedJList.add(6);
+        linkedJList.add(9);
+
+        assertTrue(linkedJList.contains(9));
+        assertFalse(linkedJList.contains(3));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetIndexOut() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        linkedJList.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveIndexOut() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        linkedJList.add(8);
+        linkedJList.add(8);
+
+        linkedJList.remove(2);
+    }
+
+    @Test
+    public void testClear() {
+
+        LinkedJList linkedJList = new LinkedJList();
+        linkedJList.add(8);
+        linkedJList.add(2);
+        linkedJList.add(3);
+
+        linkedJList.clear();
+        assertTrue(linkedJList.isEmpty());
+    }
+}
