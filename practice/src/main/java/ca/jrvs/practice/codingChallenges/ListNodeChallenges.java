3b9d895fe56e128aff2b250c7eba1a2b68e4868e
YannickBeriault <beriault.yannick@gmail.com>
2022-04-01 14:51:22 -0400

    Completed the List Cycle challenge.

--- practice/src/main/java/ca/jrvs/practice/codingChallenges/ListNodeChallenges.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/ListNodeChallenges.java
@@ -4,6 +4,33 @@ import java.util.ArrayList;
 
 public class ListNodeChallenges {
 
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode cycleMonitorNode = head;
+        ListNode currentNode = head.next;
+
+        for (int i = 0; true; i++) {
+
+            if (currentNode.next != null) {
+
+                if (currentNode.next == cycleMonitorNode)
+                    return true;
+                else {
+
+                    currentNode = currentNode.next;
+                    //Modulo 6 has no effect on lists with sizes < 8 with pos = 0 and seems to work faster
+                    // on average with a batch of test cases.
+                    if (i % 6 == 0)
+                        cycleMonitorNode = cycleMonitorNode.next;
+                }
+            } else
+                return false;
+        }
+    }
+
     public ListNode middleNode(ListNode head) {
 
         if (head == null)
--- practice/src/test/java/ca/jrvs/practice/codingChallenges/ListNodeChallengesTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenges/ListNodeChallengesTest.java
@@ -2,10 +2,24 @@ package ca.jrvs.practice.codingChallenges;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class ListNodeChallengesTest {
+
+    @Test
+    public void testHasCycle() {
+
+        ListNodeChallenges.ListNode fifth = new ListNodeChallenges.ListNode(5);
+        ListNodeChallenges.ListNode fourth = new ListNodeChallenges.ListNode(4, fifth);
+        ListNodeChallenges.ListNode third = new ListNodeChallenges.ListNode(3, fourth);
+        ListNodeChallenges.ListNode second = new ListNodeChallenges.ListNode(2, third);
+        ListNodeChallenges.ListNode head = new ListNodeChallenges.ListNode(1, second);
+        fifth.next = second;
+
+        ListNodeChallenges listNodeChallenges = new ListNodeChallenges();
+
+        assertTrue(listNodeChallenges.hasCycle(head));
+    }
 
     @Test
     public void testRemoveNthFromEnd1() {
