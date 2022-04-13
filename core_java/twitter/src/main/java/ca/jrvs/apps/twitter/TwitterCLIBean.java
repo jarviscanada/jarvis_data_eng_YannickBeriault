5f4eae11b0ebdc793ee593bf8d2324d59dbd047f
YannickBeriault <beriault.yannick@gmail.com>
2022-04-13 19:32:42 -0400

    Just trying to fix things.

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIBean.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIBean.java
@@ -12,6 +12,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
 
 @Configuration
 public class TwitterCLIBean {
