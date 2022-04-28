4daf9af342ad3c1d3de585589903fd0f8af02cb5
YannickBeriault <beriault.yannick@gmail.com>
2022-04-28 15:11:29 -0400

    Completed transcription of Quote Controller.

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -2,10 +2,14 @@ package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.Quote;
 import ca.jrvs.apps.trading.service.QuoteService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@Api(value = "quote", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Controller
@@ -20,7 +24,7 @@ public class QuoteController {
     }
 
     //@ApiOperation(value = "ShowIexQuote", notes = "Show IexQuote for a given ticker/symbol")
-    //@ApiResponse(value = {@ApiResponse(code = 404, message "ticker is not found")})
+    @ApiResponse(value = {@ApiResponse(code = 404, message "ticker is not found")})
     @GetMapping(path = "/iex/ticker/{ticker}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
@@ -35,10 +39,49 @@ public class QuoteController {
 
     @PutMapping(path = "/iexMarketData")
     @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public void updateMarketData() {
 
         try {
             quoteService.updateMarketData();
+        } catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @PutMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Quote putQuote(@RequestBody Quote quote) {
+
+        try {
+            return quoteService.saveQuote(quote);
+        } catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @PostMapping(path = "/tickerId/{tickerId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses(value = {@ApiResponse(code = 404,
+            message = "Ticker was not found in the IEX system")})
+    @ResponseBody
+    public Quote createQuote(@PathVariable String tickerId) {
+
+        try {
+            return quoteService.saveQuote(tickerId);
+        } catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @GetMapping(path = "/dailyList")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<Quote> getDailyList() {
+
+        try {
+            return quoteService.findAllQuotes();
         } catch (Exception e) {
             throw ResponseExceptionUtil.getResponseStatusException(e);
         }
